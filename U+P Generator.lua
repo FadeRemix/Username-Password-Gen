cfac8ee835488501c65a89ffae2aa02f209f2ba3
FadeRemix <64497456+FadeRemix@users.noreply.github.com>
2022-07-04 04:02:38 -0700

    Create U+P Generator.lua

---
+++ U+P Generator.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FadeRemix/Username-Password-Gen/main/Username.lua"))()
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FadeRemix/Username-Password-Gen/main/Password.lua"))()
